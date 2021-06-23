93c6e1231a8ca87f9e4382a680e5d0be12cb8b10
Anivesh nishad <78677890+Aniveshnishad76@users.noreply.github.com>
2021-06-23 14:15:28 +0530

    Update settings.py

--- education/settings.py
+++ education/settings.py
@@ -145,8 +145,8 @@ STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER="justforhost26@gmail.com"
-EMAIL_HOST_PASSWORD="Modi3008"
+EMAIL_HOST_USER="janivesh.nishad07"
+EMAIL_HOST_PASSWORD="Nishad@1234"
 EMAIL_USE_SSL=False
 
 

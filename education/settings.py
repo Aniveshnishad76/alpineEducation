8472ec90163e0a685a35ddf85ae27dd98f2b3fca
Anivesh nishad <78677890+Aniveshnishad76@users.noreply.github.com>
2021-06-24 11:19:12 +0530

    Update settings.py

--- education/settings.py
+++ education/settings.py
@@ -145,7 +145,7 @@ STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER="justforhost@gmail.com"
+EMAIL_HOST_USER="justforhost26@gmail.com"
 EMAIL_HOST_PASSWORD="Modi3008@"
 EMAIL_USE_SSL=False
 
